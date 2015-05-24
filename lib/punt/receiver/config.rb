e39c7c47a0531377dd3316d46e3c897c8a939b6a
Rodrigo Flores <mail@rodrigoflores.org>
2015-05-24 11:56:58 -0300

    Relevant repos

--- lib/punt/receiver/config.rb
+++ lib/punt/receiver/config.rb
@@ -1,12 +1,20 @@
+require 'yaml'
+
 module Punt
   module Receiver
     class Config
-      def initialize(config_file,env=ENV)
+      def initialize(config_file,env=ENV,yaml=YAML)
         @env = env
+        @yaml = yaml
       end
 
       def github_token
         @env.fetch("GITHUB_TOKEN")
+      end
+
+      def relevant_repos
+        file_name = @env.fetch("PR_REPOS_FILE")
+        @yaml.load_file(file_name)["repos"]
       end
     end
   end
--- test/punt/receiver/config_test.rb
+++ test/punt/receiver/config_test.rb
@@ -4,13 +4,22 @@ require "punt/receiver/config"
 
 describe Punt::Receiver::Config do
   before do
-    @mock = MiniTest::Mock.new
-    @config = Punt::Receiver::Config.new("a",@mock)
+    @mock_env = MiniTest::Mock.new
+    @mock_yaml = MiniTest::Mock.new
+    @config = Punt::Receiver::Config.new("a",@mock_env,@mock_yaml)
   end
 
-  
   it "gets the github token data" do
-    @mock.expect(:fetch,"a-gh-token",["GITHUB_TOKEN"])
+    @mock_env.expect(:fetch,"a-gh-token",["GITHUB_TOKEN"])
     @config.github_token.must_equal "a-gh-token"
+    assert @mock_env.verify
+  end
+
+  it "gets the relevant repos" do
+    @mock_env.expect(:fetch,"a-file.yaml", ["PR_REPOS_FILE"])
+    @mock_yaml.expect(:load_file,{"repos" => ["rails/rails"]},["a-file.yaml"])
+    @config.relevant_repos.must_equal ["rails/rails"]
+    assert @mock_env.verify
+    assert @mock_yaml.verify
   end
 end
